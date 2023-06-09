34efafecfea4c1699d60736c09ed1c7f5000bd4a
llx-mbp14 <lilongxiang2000@gmail.com>
2023-06-09 15:25:16 +0800

    style: 移除分号

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Body, Param, Query, Post} from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Body, Param, Query, Post} from '@nestjs/common'
+import { AppService } from './app.service'
+
 
 @Controller()
 export class AppController {
@@ -7,21 +8,11 @@ export class AppController {
 
   @Get()
   getHello(@Query() query?: object): string {
-    console.log(query)
     return this.appService.getHello(query)
   }
 
-  @Post()
-  postHello(@Body() body: {[key: string]: any}): {msg: string} {
-    console.log(body)
-    return {
-      msg: `hello ${body.name}`
-    }
-  }
-
   @Post('/login')
-  postLogin(@Body() body: {[key: string]: any}): {msg: string} {
-    console.log(body)
+  postLogin(@Body() body: {username: string, pwd: string}): {msg: string} {
     return {
       msg: `hello ${body.username}`
     }
