c278b9dcb1eff552e9658f4401ce4929fc75d58d
qzda <qinzhidao@foxmail.com>
2023-08-16 15:02:17 +0800

    style: 引入改为相对路径

--- src/modules/coffees/coffees.controller.ts
+++ src/modules/coffees/coffees.controller.ts
@@ -13,7 +13,7 @@ import {
 import { CoffeesService } from "./coffees.service";
 import { CreateCoffeeDto } from "./dto/create-coffee.dto";
 import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
-import { PaginationQueryDto } from "../../common/dto/pagination-query.dto";
+import { PaginationQueryDto } from "src/common/dto/pagination-query.dto";
 
 @Controller("coffees")
 export class CoffeesController {
